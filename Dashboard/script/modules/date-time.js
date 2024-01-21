cdb99134faba21eda73f5667a29b50ab363fde60
Isak Tilahun <you@example.com>
2024-01-21 12:35:41 +0100

    new Intl.DateTimeFormat() till kl i headern.

--- Dashboard/script/modules/date-time.js
+++ Dashboard/script/modules/date-time.js
@@ -2,17 +2,12 @@ export default function dateAndTime(){
   const clockEl = document.querySelector('.clock');
   const dateEl = document.querySelector('.date');
   const dateTime = new Date()
-  
-  //Time
-  const hour = (dateTime.getHours() < 10) ? '0' + dateTime.getHours() : dateTime.getHours();
-  const minutes = (dateTime.getMinutes() < 10) ? '0' + dateTime.getMinutes() : dateTime.getMinutes();
-  //Date
-  const date = new Intl.DateTimeFormat('sv', {
-    dateStyle: 'medium'
-    // timeStyle: 'medium'
-  }).format(dateTime)     
 
-  //Show time and date
-  clockEl.innerText = `Kl ${hour}:${minutes}`
+  // Format time and date
+  const time = new Intl.DateTimeFormat('sv', {timeStyle: 'short'}).format(dateTime)
+  const date = new Intl.DateTimeFormat('sv', {dateStyle: 'medium'}).format(dateTime)     
+
+  //Render time and date;
+  clockEl.innerText = `Kl ${time}`
   dateEl.innerText = date;
 }
