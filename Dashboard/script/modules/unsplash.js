6140bce01bcab1c74b17a920648920111386c955
Isak Tilahun <you@example.com>
2024-01-21 12:36:22 +0100

    unsplash api random image generator.

--- Dashboard/script/modules/unsplash.js
+++ Dashboard/script/modules/unsplash.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export default async function getRandomBackgroundImage(){
   const token = '5SCRcJdnZ8ULKdavlthtwYTp1lU1U4HMDIq3xXuEp4Y'; 
-  // const url = `https://api.unsplash.com/photos/random?client_id=${token}`
+  const url = `https://api.unsplash.com/photos/random?client_id=${token}`
   const body = document.body; 
   
   try {
-    const response = await axios(url);
+    // const response = await axios(url);
     console.log(response.data);
-    body.style.background = `url('${response.data.urls.regular}')`;
+    body.style.backgroundImage = `url('${response.data.urls.regular}')`;
   } catch (error) {
     // DEFAULT IMAGE
-    body.style.background = "url('https://images.unsplash.com/photo-1704913427603-6cb736d51f05?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+    body.style.backgroundImage = "url('https://images.unsplash.com/photo-1702234852270-8bb491075e18?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NTM5NDV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDU3NDkxNzd8&ixlib=rb-4.0.3&q=80&w=1080')"
   }
 }
